78d98bd40199d21e224801090f816576f686f5d1
Santiago-Larrosa <130399338+Santiago-Larrosa@users.noreply.github.com>
2024-06-12 12:40:31 -0300

    Update pong.cpp

--- pong.cpp
+++ pong.cpp
@@ -1,6 +1,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include <SDL2/SDL_mixer.h> // Incluir SDL_mixer
+#include <SDL2/SDL_mixer.h> 
 #include <stdio.h>
 
 #define SCREEN_WIDTH 1024
@@ -12,15 +12,18 @@
 #define FRAME_TIME 150
 #define GRAVITY 0.01
 #define JUMP_FORCE -2
+#define PLATFORM_NUM  10
 
 typedef struct {
     SDL_Rect position;
     double velocityY;
     double velocityX;
     int isJumping;
-    int isPreparingToJump; // Nuevo estado para preparar el salto
+    int isPreparingToJump;
     int jumpFrame;
     int prepareJumpFrame;
+    int isMoving;
+    int isOnGround;
 } Character;
 
 typedef struct {
@@ -40,13 +43,13 @@ int main(int argc, char* args[]) {
     SDL_Texture* spriteSheet1 = NULL;
     SDL_Texture* spriteSheet2 = NULL;
     SDL_Texture* jumpSpriteSheet = NULL;
-    SDL_Texture* fallingSpriteSheet = NULL; // Nueva textura para la caída
+    SDL_Texture* fallingSpriteSheet = NULL; 
     SDL_Texture* platformTexture = NULL;
     SDL_Rect spriteClips[SPRITES_PER_IMAGE * 4];
-    SDL_Rect platforms[5]; // Añadimos un array de plataformas
-    Mix_Music *backgroundMusic = NULL; // Música de fondo
+    SDL_Rect platforms[PLATFORM_NUM]; 
+    Mix_Music *backgroundMusic = NULL; 
 
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) { // Inicializa SDL con soporte para audio
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) { 
         printf("No se pudo inicializar SDL. SDL_Error: %s\n", SDL_GetError());
         return -1;
     }
@@ -56,55 +59,34 @@ int main(int argc, char* args[]) {
         return -1;
     }
 
-    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) { // Inicializa SDL_mixer
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) { 
         printf("No se pudo inicializar SDL_mixer. Mix_Error: %s\n", Mix_GetError());
         return -1;
     }
 
-    window = SDL_CreateWindow("Animación de Sprite",
+    window = SDL_CreateWindow("Furnace KNIGH",
                               SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
                               SCREEN_WIDTH, SCREEN_HEIGHT,
                               SDL_WINDOW_SHOWN);
-    if (window == NULL) {
-        printf("No se pudo crear la ventana. SDL_Error: %s\n", SDL_GetError());
-        return -1;
-    }
+    
 
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (renderer == NULL) {
-        printf("No se pudo crear el renderer. SDL_Error: %s\n", SDL_GetError());
-        return -1;
-    }
+   
 
     spriteSheet1 = IMG_LoadTexture(renderer, "spritesheet1.bmp");
-    if (spriteSheet1 == NULL) {
-        printf("No se pudo cargar la textura 1. SDL_Error: %s\n", SDL_GetError());
-        return -1;
-    }
+   
 
     spriteSheet2 = IMG_LoadTexture(renderer, "spritesheet2.bmp");
-    if (spriteSheet2 == NULL) {
-        printf("No se pudo cargar la textura 2. SDL_Error: %s\n", SDL_GetError());
-        return -1;
-    }
+   
 
     jumpSpriteSheet = IMG_LoadTexture(renderer, "jump_spritesheet.bmp");
-    if (jumpSpriteSheet == NULL) {
-        printf("No se pudo cargar la textura de salto. SDL_Error: %s\n", SDL_GetError());
-        return -1;
-    }
+   
 
-    fallingSpriteSheet = IMG_LoadTexture(renderer, "falling.bmp"); // Cargamos la textura de caída
-    if (fallingSpriteSheet == NULL) {
-        printf("No se pudo cargar la textura de caída. SDL_Error: %s\n", SDL_GetError());
-        return -1;
-    }
+    fallingSpriteSheet = IMG_LoadTexture(renderer, "falling.bmp"); 
+  
 
     platformTexture = IMG_LoadTexture(renderer, "platform.png");
-    if (platformTexture == NULL) {
-        printf("No se pudo cargar la textura de la plataforma. SDL_Error: %s\n", SDL_GetError());
-        return -1;
-    }
+    
 
     for (int i = 0; i < SPRITES_PER_IMAGE; ++i) {
         spriteClips[i].x = i * SPRITE_WIDTH;
@@ -145,15 +127,22 @@ int main(int argc, char* args[]) {
     player.isPreparingToJump = 0;
     player.jumpFrame = 0;
     player.prepareJumpFrame = 0;
+    player.isMoving = 0;
+    player.isOnGround = 1; 
 
-    Camera camera = {0, 0};
+    Camera camera = {0-(SPRITE_WIDTH/6), 0};
 
-    // Inicializamos varias plataformas
-    platforms[0] = (SDL_Rect){0, SCREEN_HEIGHT - PLATFORM_HEIGHT, SCREEN_WIDTH, PLATFORM_HEIGHT};
-    platforms[1] = (SDL_Rect){200, SCREEN_HEIGHT - 200, 200, PLATFORM_HEIGHT};
-    platforms[2] = (SDL_Rect){500, SCREEN_HEIGHT - 300, 200, PLATFORM_HEIGHT};
-    platforms[3] = (SDL_Rect){800, SCREEN_HEIGHT - 400, 200, PLATFORM_HEIGHT};
-    platforms[4] = (SDL_Rect){1100, SCREEN_HEIGHT - 200, 200, PLATFORM_HEIGHT};
+    
+    platforms[0] = (SDL_Rect){0, SCREEN_HEIGHT - PLATFORM_HEIGHT, SCREEN_WIDTH*10, PLATFORM_HEIGHT};
+    platforms[1] = (SDL_Rect){200, SCREEN_HEIGHT - 200, 300, PLATFORM_HEIGHT};
+    platforms[2] = (SDL_Rect){500, SCREEN_HEIGHT - 300, 300, PLATFORM_HEIGHT};
+    platforms[3] = (SDL_Rect){800, SCREEN_HEIGHT - 400, 300, PLATFORM_HEIGHT};
+    platforms[4] = (SDL_Rect){1100, SCREEN_HEIGHT - 200, 300, PLATFORM_HEIGHT};
+    platforms[5] = (SDL_Rect){1400, SCREEN_HEIGHT - 300, 300, PLATFORM_HEIGHT};
+    platforms[6] = (SDL_Rect){1700, SCREEN_HEIGHT - 100, 250, PLATFORM_HEIGHT};
+    platforms[7] = (SDL_Rect){1900, SCREEN_HEIGHT - 300, 50, PLATFORM_HEIGHT};
+    platforms[8] = (SDL_Rect){2300, SCREEN_HEIGHT - 400, 200, PLATFORM_HEIGHT};
+    platforms[9] = (SDL_Rect){2500, SCREEN_HEIGHT - 100, 300, PLATFORM_HEIGHT};
 
     int currentFrame = 0;
     SDL_Event e;
@@ -162,7 +151,7 @@ int main(int argc, char* args[]) {
     int isMoving = 0;
     int direction = 0;
 
-    // Cargar la música de fondo
+   
     backgroundMusic = Mix_LoadMUS("background_music.mp3");
     if (backgroundMusic == NULL) {
         printf("No se pudo cargar la música de fondo. Mix_Error: %s\n", Mix_GetError());
@@ -185,14 +174,12 @@ int main(int argc, char* args[]) {
                         if (!player.isPreparingToJump && !player.isJumping) {
                             direction = -1;
                             isMoving = 1;
-                            player.velocityY += GRAVITY;
                         }
                         break;
                     case SDLK_RIGHT:
                         if (!player.isPreparingToJump && !player.isJumping) {
                             direction = 1;
                             isMoving = 1;
-                            player.velocityY += GRAVITY;
                         }
                         break;
                     case SDLK_UP:
@@ -201,7 +188,15 @@ int main(int argc, char* args[]) {
                             player.prepareJumpFrame = 0;
                         }
                         break;
+                    case SDLK_r:
+                       player.position.y = 0;
+                       player.position.x = 0;
+                       player.isOnGround = 0;
+                       player.isJumping = 0;
+                       player.velocityY = -1;
+                        break;
                 }
+                
             } else if (e.type == SDL_KEYUP) {
                 switch (e.key.keysym.sym) {
                     case SDLK_LEFT:
@@ -222,7 +217,7 @@ int main(int argc, char* args[]) {
         }
 
         int wasFalling = player.velocityY > 0;
-
+      
         if (player.isJumping) {
             player.velocityY += GRAVITY;
             player.position.y += player.velocityY;
@@ -233,25 +228,34 @@ int main(int argc, char* args[]) {
                 startTime = currentTime;
             }
 
-            for (int i = 0; i < 5; ++i) {
+            for (int i = 0; i < PLATFORM_NUM; ++i) {
                 if (checkCollision(player.position, platforms[i])) {
                     player.velocityY = 0;
                     player.position.y = platforms[i].y - player.position.h;
                     player.isJumping = 0;
+                    player.isOnGround = 1;
                     break;
                 }
             }
-        } else if (player.isPreparingToJump) {
-            Uint32 currentTime = SDL_GetTicks();
-            if (currentTime - startTime > FRAME_TIME) {
-                player.prepareJumpFrame = (player.prepareJumpFrame + 1) % (SPRITES_PER_IMAGE / 2); // Asume que la primera mitad es para preparar el salto
-                startTime = currentTime;
+        } 
+        else if (!player.isOnGround) {
+            player.velocityY += GRAVITY;
+            player.position.y += player.velocityY;
+
+            for (int i = 0; i < PLATFORM_NUM; ++i) {
+                if (checkCollision(player.position, platforms[i])) {
+                    player.velocityY = 0;
+                    player.position.y = platforms[i].y - player.position.h;
+                    player.isOnGround = 1;
+                    break;
+                }
             }
         }
 
         if (isMoving && !player.isPreparingToJump) {
             player.position.x += direction * player.velocityX;
             camera.x += direction * player.velocityX;
+
             Uint32 currentTime = SDL_GetTicks();
             if (currentTime - startTime > FRAME_TIME) {
                 currentFrame = (currentFrame + 1) % (SPRITES_PER_IMAGE * 2);
@@ -259,6 +263,22 @@ int main(int argc, char* args[]) {
             }
         } else {
             currentFrame = 0;
+        }
+
+        // Verificación del borde de la plataforma
+        if (player.isOnGround) {
+            int onPlatform = 0;
+            for (int i = 0; i < PLATFORM_NUM; ++i) {
+                if (checkCollision(player.position, platforms[i])) {
+                    onPlatform = 1;
+                    break;
+                }
+            }
+            if (!onPlatform) {
+                player.isOnGround = 0;
+                
+                player.velocityY = GRAVITY;
+            }
         }
 
         if (player.position.x < SCREEN_WIDTH / 2) {
@@ -273,7 +293,7 @@ int main(int argc, char* args[]) {
 
         SDL_Rect renderPlayerRect = {player.position.x - camera.x, player.position.y, player.position.w, player.position.h};
 
-        for (int i = 0; i < 5; ++i) {
+        for (int i = 0; i < PLATFORM_NUM; ++i) {
             SDL_Rect renderPlatformRect = {platforms[i].x - camera.x, platforms[i].y, platforms[i].w, platforms[i].h};
             SDL_RenderCopy(renderer, platformTexture, NULL, &renderPlatformRect);
         }
@@ -282,19 +302,26 @@ int main(int argc, char* args[]) {
         SDL_Rect* currentClip;
 
         if (player.isJumping) {
-            if (player.velocityY > 0) { // El personaje está cayendo
+            if (player.velocityY > 0) { 
                 currentSpriteSheet = fallingSpriteSheet;
-                renderPlayerRect.w = 504 / 6; // Ajustamos el tamaño para la textura de caída
-                renderPlayerRect.h = 495 / 6; // Ajustamos el tamaño para la textura de caída
-                currentClip = NULL; // Renderizamos toda la textura
+                renderPlayerRect.w = 504 / 6; 
+                renderPlayerRect.h = 495 / 6; 
+                currentClip = NULL; 
             } else {
                 currentSpriteSheet = jumpSpriteSheet;
-                renderPlayerRect.w = SPRITE_WIDTH / 3; // Restauramos el tamaño para otras texturas
-                renderPlayerRect.h = SPRITE_HEIGHT / 3; // Restauramos el tamaño para otras texturas
+                renderPlayerRect.w = SPRITE_WIDTH / 3; 
+                renderPlayerRect.h = SPRITE_HEIGHT / 3; 
                 currentClip = &spriteClips[player.jumpFrame + 2 * SPRITES_PER_IMAGE];
             }
         } else if (player.isPreparingToJump) {
             currentSpriteSheet = jumpSpriteSheet;
+            renderPlayerRect.w = SPRITE_WIDTH / 3; 
+            renderPlayerRect.h = SPRITE_HEIGHT / 3; 
+            Uint32 currentTime = SDL_GetTicks();
+            if (currentTime - startTime > FRAME_TIME) {
+                player.prepareJumpFrame = (player.prepareJumpFrame + 1) % 2;  // Solo usar los primeros dos frames
+                startTime = currentTime;
+            }
             currentClip = &spriteClips[player.prepareJumpFrame];
         } else {
             currentSpriteSheet = (currentFrame < SPRITES_PER_IMAGE) ? spriteSheet1 : spriteSheet2;
@@ -307,14 +334,13 @@ int main(int argc, char* args[]) {
         SDL_RenderPresent(renderer);
     }
 
-    // Liberar recursos de música
     Mix_FreeMusic(backgroundMusic);
     backgroundMusic = NULL;
 
     SDL_DestroyTexture(spriteSheet1);
     SDL_DestroyTexture(spriteSheet2);
     SDL_DestroyTexture(jumpSpriteSheet);
-    SDL_DestroyTexture(fallingSpriteSheet); // Destruir la textura de caída
+    SDL_DestroyTexture(fallingSpriteSheet); 
     SDL_DestroyTexture(platformTexture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
@@ -324,3 +350,4 @@ int main(int argc, char* args[]) {
 
     return 0;
 }
+//gcc -o game game.cpp -lSDL2 -lSDL2_image -lSDL2_mixer
